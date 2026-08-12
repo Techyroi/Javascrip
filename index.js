c6a04a2f77ae80b2c3088bd62f1b84465ef2825c
Abdulrasaq Shuaib <abdulrasaq4omoyeni@gmail.com>
2026-08-12 21:44:14 +0100

    initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function validate(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var errorText = document.getElementById('error-id');
+    var message;
+
+
+    errorText.style.padding = '20px';
+
+
+    if(name.length < 3){
+        message = "Enter a valid Name"
+        errorText.innerHTML = message
+        return false
+    }
+
+    
+    if(email.indexOf('@') == -1 || email.length < 7){
+        message = "Enter a valid Email Address"
+        errorText.innerHTML = message
+        return false
+    }
+
+    
+    if(isNaN(phone) || phone.length < 11){
+        message = "Enter a valid Phone Number"
+        errorText.innerHTML = message
+        return false
+    }
+
+    alert('form submitted successfuly')
+
+
+    return true
+}
